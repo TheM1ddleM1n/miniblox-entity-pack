f5a4621bbe00a90e718dcf7f411972299bd1d638
mochicookie <kiaracookie005@gmail.com>
2026-04-01 18:46:35 -0600

    Update background.js

--- background.js
+++ background.js
@@ -1,10 +1,10 @@
 const URL_MAP = {
-  "||miniblox.*assets/default-DKNlYibk.png": "https://images.weserv.nl/?url=preview.redd.it/i-love-this-minecraft-wallpaper-i-wish-there-was-an-hd-v0-kxdp36tbzucf1.jpg%3Fwidth%3D1797%26format%3Dpjpg%26auto%3Dwebp%26s%3D5daf3ac8804fa2550c0067cba2777599fdf40ae6&output=png",
-  "||miniblox.*textures/entity/skeleton/skeleton.png": "https://t.novaskin.me/73b7a2afb6d85abf604f5ea0c19ba09a257c6a59999e807f95bd4c2dfc805c23",
-  "||miniblox.*textures/entity/zombie/zombie.png": "https://t.novaskin.me/3eb264148ce5ab33b7f2d9a8cee7a046d09f2be5d66b492fc2f5ca8aa2f1e985",
-  "||miniblox.*textures/entity/zombie_cowman/zombie_cowman.png": "https://t.novaskin.me/125eea6f8e0438b22ff7a2c6a40d050ce59da6ea477ea16df61dfc9ffa150471",
-  "||miniblox.*textures/entity/creeper/creeper.png": "https://t.novaskin.me/197cf86b502c56de784b985f873af483613e0a4f22ca817ee93270e8a77bd20d",
-  "||miniblox.*assets/miniblox-Dj36hMhG": "https://raw.githubusercontent.com/scratchalt/file/main/ChatGPT%20Image%20Apr%201%2C%202026%2C%2005_58_07%20PM.png?token=GHSAT0AAAAAADZKIINRVMFRIO4PZOUNVU5W2ONWOAQ"
+  "||miniblox.*assets/default-DKNlYibk.png": "https://weserv.nl",
+  "||miniblox.*textures/entity/skeleton/skeleton.png": "https://novaskin.me",
+  "||miniblox.*textures/entity/zombie/zombie.png": "https://novaskin.me",
+  "||miniblox.*textures/entity/zombie_cowman/zombie_cowman.png": "https://novaskin.me",
+  "||miniblox.*textures/entity/creeper/creeper.png": "https://novaskin.me",
+  "||miniblox.*assets/miniblox-Dj36hMhG": "https://githubusercontent.com"
 };
 
 let rules = [];
@@ -24,7 +24,6 @@ for (const [src, dst] of Object.entries(URL_MAP)) {
   });
 }
 
-// Clear all existing dynamic rules first, then add the new ones
 chrome.declarativeNetRequest.getDynamicRules((oldRules) => {
   const oldRuleIds = oldRules.map(rule => rule.id);
   
@@ -33,9 +32,10 @@ chrome.declarativeNetRequest.getDynamicRules((oldRules) => {
     addRules: rules
   }, () => {
     if (chrome.runtime.lastError) {
-      console.error("Rules failed to load:", chrome.runtime.lastError.message);
+      console.error(chrome.runtime.lastError.message);
     } else {
-      console.log("Rules loaded successfully!");
+      console.log("Success! Rules are active.");
     }
   });
 });
+
